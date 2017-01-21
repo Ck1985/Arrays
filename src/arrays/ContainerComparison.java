58009c0e53616d2ad39a4ee6a6dec5e6576a0d50
cunkin1985 <anonymous.vn1985@gmail.com>
2017-01-21 22:06:29 +0700

    new Commit

--- src/arrays/ContainerComparison.java
+++ src/arrays/ContainerComparison.java
@@ -2,5 +2,35 @@ package arrays;
 /**
  * Created by anonymous on 1/21/2017.
  */
+import java.util.*;
+
+class BerylliumSphere{
+    private static long count = 0;
+    private final long id = count++;
+    public String toString(){
+        return "Sphere: " + this.id;
+    }
+}
+
 public class ContainerComparison {
+    public static void main(String[] args){
+        BerylliumSphere[] spheres = new BerylliumSphere[10];
+        for(int i = 0; i < 5; i++){
+            spheres[i] = new BerylliumSphere();
+        }
+        System.out.println(Arrays.toString(spheres));
+        System.out.println(spheres[4]);
+        List<BerylliumSphere> sphereList = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            sphereList.add(new BerylliumSphere());
+        }
+        System.out.println(sphereList);
+        System.out.println(sphereList.get(4));
+        Integer[] integers = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(integers));
+        System.out.println(integers[4]);
+        List<Integer> integerList = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        System.out.println(integerList);
+        System.out.println(integerList.get(4));
+    }
 }
