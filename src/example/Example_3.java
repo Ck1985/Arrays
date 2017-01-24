dbb7a674e6f3c398fe233404f8a5f6947e43dea5
cunkin1985 <anonymous.vn1985@gmail.com>
2017-01-24 16:45:32 +0700

    new Commit

---
+++ src/example/Example_3.java
@@ -0,0 +1,21 @@
+package example;
+
+/**
+ * Created by anonymous on 1/24/2017.
+ */
+import java.util.*;
+
+public class Example_3 {
+    public static void construcArray(int sizeArray, double begin, double end){
+        double[][] twoDimArray = new double[sizeArray][];
+        for(int i = 0; i < sizeArray; i++){
+            twoDimArray[i] = new double[sizeArray];
+            for(double j = begin; j <= end; j++){
+                twoDimArray[i][j] =
+            }
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
